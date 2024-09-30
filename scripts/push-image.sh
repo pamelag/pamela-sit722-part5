53e70cefcc1b51bb5547dc2df631df34dc15dda8
pamelag <pamela.gangopadhyay@gmail.com>
2024-09-30 20:48:53 +1000

    added docker login

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -6,4 +6,5 @@ set -u # or set -o nounset
 : "$REGISTRY_PW"
 : "$SERVICE_API"
 
+echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
 docker push $CONTAINER_REGISTRY/$SERVICE_API:$VERSION
